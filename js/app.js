724572f1ba2e02159ea4534996d492083ca7b3a8
aiolos <15988346908@163.com>
2019-03-20 11:12:40 +0800

    login and register

---
+++ js/app.js
@@ -0,0 +1,36 @@
+window.app = {
+	
+	serverUrl: "http://192.168.3.255:8080",
+	
+	/**
+	 * 判断字符串是否为空
+	 * true：不为空
+	 * false：为空
+	 */
+	isNotNull: function(str) {
+		if (str != null && str != "" && str != undefined) {
+			return true;
+		}
+		return false;
+	},
+	/**
+	 * 封装消息提示框，默认mui的不支持居中和自定义icon，所以使用h5+
+	 */
+	showToast: function(msg, type) {
+		plus.nativeUI.toast(msg, {icon: "image/" + type + ".png", verticalAlign: "center"})
+	},
+	/**
+	 * 获取用户的全局对象
+	 */
+	getUserGlobalInfo: function() {
+		var userInfoStr = plus.storage.getItem("userInfo");
+		return JSON.parse(userInfoStr);
+	},
+	/**
+	 * 保存用户的全局对象
+	 */
+	setUserGlobalInfo: function(user) {
+		var userInfoStr = JSON.stringify(user);
+		plus.storage.setItem("userInfo", userInfoStr);
+	}
+}
